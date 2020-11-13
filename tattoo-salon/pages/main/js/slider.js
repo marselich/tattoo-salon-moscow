79c0671868bbf2557f259aee21183691d2305d67
marsell72 <70841587+marsell72@users.noreply.github.com>
2020-11-13 11:33:27 +0500

    feat: add style for all wrappers in main

---
+++ tattoo-salon/pages/main/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   /*  $('.stock-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    }); */
+});
